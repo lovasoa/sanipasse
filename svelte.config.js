f408d19ed57efa30e26a23fc711f1560e48ce949
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2021-06-07 23:02:01 +0200

    higher chunk size limit

--- svelte.config.js
+++ svelte.config.js
@@ -16,8 +16,14 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter
-	}
+		adapter,
+		vite: {
+			build: {
+				chunkSizeWarningLimit: 700
+			}
+		}
+	},
+
 };
 
 export default config;
