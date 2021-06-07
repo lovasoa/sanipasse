d59a8f893065e283fea383bf777a362605e91286
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2021-06-08 01:15:27 +0200

    add cache network timeout

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -19,11 +19,12 @@ precacheAndRoute(all_assets);
 // Cache page navigations (html) with a Network First strategy
 registerRoute(
     // Check to see if the request is a navigation to a new page
-    ({ request }) => request.mode === 'navigate',
+    ({ request }) => request.mode === 'navigate' || request.destination === 'document',
     // Use a Network First caching strategy
     new NetworkFirst({
         // Put all cached files in a cache named 'pages'
         cacheName: 'pages',
+        networkTimeoutSeconds: 5,
         plugins: [
             // Ensure that only requests that result in a 200 status are cached
             new CacheableResponsePlugin({ statuses: [200] }),
