7d459a5f7bc2e89fdf0267b7817edca596db3a7d
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2021-06-07 21:52:15 +0200

    Adapt to latest sveltekit
    see https://github.com/sveltejs/kit/issues/1623

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,11 +1,11 @@
 import type { Handle } from '@sveltejs/kit';
-export const handle: Handle = async ({ request, render }) => {
+export const handle: Handle = async ({ request, resolve }) => {
     const id = (Math.random() * 100) | 0;
     const start = performance.now();
     const referer = request.headers['referer'];
     const referer_text = referer ? `(from: ${referer})` : '';
     console.log(`${request.method} ${request.path} ${referer_text} [${id}]`);
-    const response = await render(request);
+    const response = await resolve(request);
     const ms = (performance.now() - start).toFixed(1);
     const kb = ((response.body?.length || 0) / 1000).toFixed(2);
     console.log(`${request.path}: ${response.status} (${kb} kb, ${ms} ms) [${id}]`);
