732a570c08d12d35f07e81a526f20e7a2bb494df
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2021-06-07 23:51:34 +0200

    Mailleure mise en cache

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,7 +1,37 @@
-import { precacheAndRoute } from 'workbox-precaching';
-import { build, timestamp } from '$service-worker';
+import { registerRoute } from 'workbox-routing';
+import { NetworkFirst } from 'workbox-strategies';
 
-precacheAndRoute(build.map(url => ({
+// Used for filtering matches based on status code, header, or both
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+// Used to limit entries in cache, remove entries after a certain period of time
+import { ExpirationPlugin } from 'workbox-expiration';
+
+import { precacheAndRoute } from 'workbox-precaching';
+import { build, files, timestamp } from '$service-worker';
+
+const all_assets = [...build, ...files].map(url => ({
     url,
     revision: new Date(timestamp).toISOString()
-})));
+}));
+
+precacheAndRoute(all_assets);
+
+// Cache page navigations (html) with a Network First strategy
+registerRoute(
+    // Check to see if the request is a navigation to a new page
+    ({ request }) => request.mode === 'navigate',
+    // Use a Network First caching strategy
+    new NetworkFirst({
+        // Put all cached files in a cache named 'pages'
+        cacheName: 'pages',
+        plugins: [
+            // Ensure that only requests that result in a 200 status are cached
+            new CacheableResponsePlugin({ statuses: [200] }),
+            // Don't cache more than 10 items, and expire them after 7 days
+            new ExpirationPlugin({
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 7, // 7 Days
+            }),
+        ],
+    }),
+);
