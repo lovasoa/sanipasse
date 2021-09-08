1a0dfe1ded0d009cc9c4a0fba2c17ccf1d8552b2
lovasoa <pere.jobs@gmail.com>
2021-09-08 13:26:07 +0200

    améliorations de l'affichage des statistiques

--- src/routes/borne/_stats_chart.svelte
+++ src/routes/borne/_stats_chart.svelte
@@ -1,32 +1,70 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { current_time_bucket, STATS_GRANULARITY_MILLIS } from './_stats_storage';
 	import type { StatsDataPoint } from './_stats_storage';
 	import type ApexCharts from 'apexcharts';
+
+	export let datapoints: StatsDataPoint[] = [];
+	export let show_toolbar = true;
+
 	type Class<T = any> = new (...args: any[]) => T;
 
 	let apex: ApexCharts | null = null;
 	let ApexCharts: Class<ApexCharts> | null = null;
 
 	let chart: HTMLDivElement | null = null;
-	export let datapoints: StatsDataPoint[] = [];
+
+	const MIN_DURATION_MILLIS = STATS_GRANULARITY_MILLIS * 24;
+
+	function compute_datapoints_with_zeroes(datapoints: StatsDataPoint[]): StatsDataPoint[] {
+		const end_timestamp = datapoints[0]?.date?.getTime() || current_time_bucket();
+		const start_or_null = datapoints[datapoints.length - 1]?.date?.getTime();
+		let start_timestamp = end_timestamp - MIN_DURATION_MILLIS;
+		if (start_or_null < start_timestamp) start_timestamp = start_or_null;
+		const num_points = (end_timestamp - start_timestamp) / STATS_GRANULARITY_MILLIS;
+		const datapoints_with_zeroes = new Array(num_points).fill(null).map((_, i) => ({
+			date: new Date(start_timestamp + i * STATS_GRANULARITY_MILLIS),
+			valid: 0,
+			invalid: 0
+		}));
+		for (const dp of datapoints) {
+			const idx = ((dp.date.getTime() - start_timestamp) / STATS_GRANULARITY_MILLIS) | 0;
+			if (idx < 0 || idx > datapoints_with_zeroes.length)
+				console.log(idx, dp, start_timestamp, end_timestamp);
+			datapoints_with_zeroes[idx] = dp;
+		}
+		return datapoints_with_zeroes;
+	}
+	$: datapoints_with_zeroes = compute_datapoints_with_zeroes(datapoints);
 	let series: ApexAxisChartSeries = [];
 	$: series = [
 		{
-			name: 'Validé',
+			name: 'validés',
 			color: '#20c997', // green
-			data: datapoints.map((d) => ({ x: d.date.getTime(), y: d.valid }))
+			data: datapoints_with_zeroes.map((d) => ({ x: d.date.getTime(), y: d.valid }))
 		},
 		{
-			name: 'Refusé',
+			name: 'refusés',
 			color: '#dc3545', // red
-			data: datapoints.map((d) => ({ x: d.date.getTime(), y: d.invalid }))
+			data: datapoints_with_zeroes.map((d) => ({ x: d.date.getTime(), y: d.invalid }))
 		}
 	];
-	const options = {
+	const options: ApexCharts.ApexOptions = {
 		series: [],
 		chart: {
 			height: 150,
-			type: 'area'
+			type: 'area',
+			toolbar: {
+				show: show_toolbar,
+				export: {
+					csv: {
+						dateFormatter: (d) => (d ? new Date(d).toISOString() : ''),
+						filename: 'statistiques_sanipasse.csv',
+						columnDelimiter: ';',
+						headerCategory: 'date'
+					}
+				}
+			}
 		},
 		dataLabels: {
 			enabled: false
@@ -36,15 +74,16 @@
 		},
 		xaxis: {
 			type: 'datetime',
-			labels: {
-				show: false
-			}
+			tooltip: { enabled: false }
 		},
 		yaxis: {
 			labels: {
 				formatter(val: number) {
 					return val.toFixed(0);
 				}
+			},
+			tooltip: {
+				enabled: false
 			}
 		},
 		legend: {
@@ -54,6 +93,8 @@
 			x: {
 				formatter: (timestamp: number) =>
 					new Date(timestamp).toLocaleString('fr-FR', {
+						month: 'short',
+						day: 'numeric',
 						hour: 'numeric',
 						minute: 'numeric'
 					})
@@ -65,7 +106,6 @@
 		apex.render();
 	}
 	$: if (apex) apex.updateSeries(series);
-	$: console.log(series);
 
 	onMount(async () => {
 		ApexCharts = (await import('apexcharts')).default;
