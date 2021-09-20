115af31a8208c4044d20918bedab0683902507e3
lovasoa <pere.jobs@gmail.com>
2021-09-20 10:50:16 +0200

    Add a workaround for misconfigured scanners

--- src/routes/borne/_scan_stats_modal.svelte
+++ src/routes/borne/_scan_stats_modal.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 	import { Button, Modal, ModalBody, ModalFooter, ModalHeader, Icon } from 'sveltestrap';
 	import Statistiques from './statistiques.svelte';
+	let btn: HTMLButtonElement | null = null;
 	let open = false;
-	const toggle = () => (open = !open);
+	function toggle() {
+		open = !open;
+		// Don't keep the focus on the button, so that if a misconfigured scanner emits a "return" key event, it doesn't reopen the modal.
+		if (btn) btn.blur();
+	}
 </script>
 
 <button
 	class="fixed-bottom btn btn-outline-secondary"
 	title="statistiques d'utilisation"
+	bind:this={btn}
 	on:click={toggle}><Icon name="bar-chart-line" /></button
 >
 
