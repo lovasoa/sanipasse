1a7f31a6d0760011a6fd635e14fee942f3f737de
lovasoa <pere.jobs@gmail.com>
2022-01-04 09:01:35 +0000

    Fix #145

---
+++ src/lib/TooltipFix.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	// https://github.com/bestguy/sveltestrap/issues/414
+	import { Tooltip } from 'sveltestrap';
+
+	let target: HTMLElement | null = null;
+</script>
+
+<span bind:this={target}><slot name="target" /></span>
+{#if target}
+	<Tooltip {target}><slot /></Tooltip>
+{/if}
