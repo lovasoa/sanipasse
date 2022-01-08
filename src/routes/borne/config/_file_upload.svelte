ac67f2a8e296baa10cfeeb4b27d1e2d5aac2d059
lovasoa <pere.jobs@gmail.com>
2022-01-08 17:33:38 +0000

    meilleur message de chargement

--- src/routes/borne/config/_file_upload.svelte
+++ src/routes/borne/config/_file_upload.svelte
@@ -2,36 +2,38 @@
 	import b64 from 'base64-js';
 	import { ALLOWED_FILE_TYPES, MAX_FILESIZE } from '$lib/global_config';
 	import { _ } from 'ajv';
+	import ShowPromiseError from '../../_showPromiseError.svelte';
 
 	export let label = 'Fichiers';
 	export let file_urls: string[];
 	export let allowed_types = ['image', 'video'];
 
 	let fileInput: HTMLInputElement | null = null;
+
+	let loading: Promise<void> = Promise.resolve();
+
 	async function updateLogosUrls() {
 		if (!fileInput) throw new Error('missing file element');
 		const { files } = fileInput;
 		if (!files || !files.length) throw new Error('No file in input');
-		const files_buffers = Array.from(files).map((file) => ({
-			file,
-			arrayBuffer: file.arrayBuffer()
-		}));
-		if (files_buffers.some((x) => x.file.size > MAX_FILESIZE)) {
-			alert(`Les fichiers de plus de ${(MAX_FILESIZE / 1e6).toFixed(1)} Mo ne sont pas autorisés.`);
-			return resetFiles();
-		}
+		resetFiles();
 		file_urls = [];
-		for (const { file, arrayBuffer } of files_buffers) {
-			const buffer = await arrayBuffer;
-			const bytes = new Uint8Array(buffer);
-			const url = `data:${file.type};base64,${b64.fromByteArray(bytes)}`;
-			file_urls.push(url);
-			file_urls = file_urls; // refresh
+		for (const url of Array.from(files).map(file_to_url)) {
+			file_urls = [...file_urls, await url];
 		}
 	}
+
+	async function file_to_url(file: File): Promise<string> {
+		if (file.size > MAX_FILESIZE)
+			throw `Les fichiers de plus de ${(MAX_FILESIZE / 1e6).toFixed(1)} Mo ne sont pas autorisés.`;
+		const buffer = await file.arrayBuffer();
+		const bytes = new Uint8Array(buffer);
+		return `data:${file.type};base64,${b64.fromByteArray(bytes)}`;
+	}
+
 	function resetFiles() {
 		file_urls = [];
-		if (fileInput) fileInput.value = '';
+		setTimeout(() => fileInput && (fileInput.value = ''), 0);
 	}
 </script>
 
@@ -43,7 +45,7 @@
 				type="file"
 				class="form-control"
 				bind:this={fileInput}
-				on:change={updateLogosUrls}
+				on:change={() => (loading = updateLogosUrls())}
 				accept={Object.entries(ALLOWED_FILE_TYPES)
 					.flatMap(([extension, type]) =>
 						allowed_types.some((t) => type.startsWith(t)) ? ['.' + extension, type] : []
@@ -53,6 +55,9 @@
 				id="bgimage"
 			/>
 		</div>
+	</div>
+	<div class="col-12 mb-3">
+		<ShowPromiseError promise={loading} />
 	</div>
 	<div class="col-12 mb-3">
 		{#each file_urls as url}
